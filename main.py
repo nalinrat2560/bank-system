e222c4a1b57ce17829bfeb9adf4b8602c93d5a80
nalinrat2560 <109011322+nalinrat2560@users.noreply.github.com>
2025-12-29 20:47:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 class Account:
-    def __init__(self, ower, balance=0):
-        self.ower = ower
+    def __init__(self, owner, balance=0):
+        self.owner = owner
         self.balance = balance
 
     def deposit(self, amount):
